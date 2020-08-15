a30f4fc7994dd57c64e9d6c978f9227273c6c829
Jeff Higham <jeff@f3code.com>
2020-08-15 08:10:03 -0400

    Initial Commit

---
+++ SwiftUIProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Jeff Higham on 8/15/20.
+//  Copyright © 2020 F3 Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
